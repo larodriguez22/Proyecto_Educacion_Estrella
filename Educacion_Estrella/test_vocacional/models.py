ae84607e05149147467755be836e6c19b56d43ec
Germán Rojas <g.rojasc@uniandes.edu.co>
2022-04-01 10:06:58 -0500

    Actualización atributos

--- Educacion_Estrella/test_vocacional/models.py
+++ Educacion_Estrella/test_vocacional/models.py
@@ -1,6 +1,6 @@
 from email.policy import default
-from unittest import result
 from django.db import models
+from pregunta.models import Pregunta
 
 # Create your models here.
 
@@ -8,4 +8,9 @@ class Test_Vocacional(models.Model):
     idTest = models.IntegerField(null = True, default = None)
     finalResult = None
     testDate = models.DateTimeField(auto_now_add=True)
+    questions = models.ForeignKey(Pregunta, on_delete=models.CASCADE, default=None)
 
+    def __str__(self):
+        return '%s %s %s' % (self.idTest, self.finalResult, self.testDate)
+        return '{}'.format(self.name)
+        
