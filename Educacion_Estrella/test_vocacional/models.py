037c317918d3b60dcab8287136570a537a2fb57e
Germán Rojas <g.rojasc@uniandes.edu.co>
2022-04-01 09:30:47 -0500

    Creación concepto Estudiante

--- Educacion_Estrella/test_vocacional/models.py
+++ Educacion_Estrella/test_vocacional/models.py
@@ -1,3 +1,11 @@
+from email.policy import default
+from unittest import result
 from django.db import models
 
 # Create your models here.
+
+""" class Test_Vocacional(models.Model):
+    idTest = models.models.IntegerField(null = True, default = None)
+    finalResult = None
+    testDate = None
+ """
