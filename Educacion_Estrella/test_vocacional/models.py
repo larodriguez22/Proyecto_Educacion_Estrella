d0df16f72ff6169b11b7ede229301ba895df19a9
Germán Rojas <g.rojasc@uniandes.edu.co>
2022-04-01 10:43:08 -0500

    Implementación de modelos y añadir atributos

--- Educacion_Estrella/test_vocacional/models.py
+++ Educacion_Estrella/test_vocacional/models.py
@@ -5,12 +5,10 @@ from pregunta.models import Pregunta
 # Create your models here.
 
 class Test_Vocacional(models.Model):
-    idTest = models.IntegerField(null = True, default = None)
     finalResult = None
     testDate = models.DateTimeField(auto_now_add=True)
-    questions = models.ForeignKey(Pregunta, on_delete=models.CASCADE, default=None)
+    questions = models.ManyToManyField(Pregunta, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
-        return '%s %s %s' % (self.idTest, self.finalResult, self.testDate)
+        return '%s %s' % (self.finalResult, self.testDate)
         return '{}'.format(self.name)
-        
