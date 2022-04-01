013dfcab364a38d2c75bcab659840e898de01a03
Germán Rojas <g.rojasc@uniandes.edu.co>
2022-04-01 09:41:12 -0500

    Creación concepto pregunta

--- Educacion_Estrella/test_vocacional/models.py
+++ Educacion_Estrella/test_vocacional/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 # Create your models here.
 
-""" class Test_Vocacional(models.Model):
-    idTest = models.models.IntegerField(null = True, default = None)
+class Test_Vocacional(models.Model):
+    idTest = models.IntegerField(null = True, default = None)
     finalResult = None
-    testDate = None
- """
+    testDate = models.DateTimeField(auto_now_add=True)
+
