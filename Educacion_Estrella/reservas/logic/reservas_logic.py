6a139399d767b143cca6877727705ddbdf926f8e
Wilton2612 <wiltonestebanm2001@gmail.com>
2022-05-14 02:25:20 -0500

    ASR integridad 2

--- Educacion_Estrella/reservas/logic/reservas_logic.py
+++ Educacion_Estrella/reservas/logic/reservas_logic.py
@@ -10,22 +10,11 @@ def get_muchas():
     return reservas
 
 
-
-"""
-def get_reservas(nombre):
-
-    reserva =Reserva.objects.get(pk=nombre)
-    #reserva = Reserva.objects.raw('SELECT * FROM reservas_reserva WHERE id=%s' % ids)[0]
-    return reserva
-"""
-
 def get_reservas(id):
 
     print(id)
     #reserva = Reserva.objects.raw('SELECT * FROM reservas_reserva WHERE id = %s;', int(id))
    
-
-
     try:
         reserva = Reserva.objects.raw("SELECT * FROM reservas_reserva WHERE id = '%s'" % id)[0]
         print(reserva)
@@ -33,23 +22,4 @@ def get_reservas(id):
 
     except:
         return False
-
-
-    #UPDATE reservas_reserva SET estudiante='maria';--
-
-
-"""
-def get_reservas(request):
-    if request.is_ajax():
-        search_term = request.POST.get('searchTerm')
-
-        # Searching employees using Django ORM - the best way
-        employees = Employee.objects.filter(mentor__icontains=search_term)
-
-        #Searching employees using raw() - the correct way
-        reservas = Reserva.objects.raw('SELECT * FROM Reserva WHERE first_name ILIKE %s;',[search_term])
-
-        return reservas
-
-"""
 
