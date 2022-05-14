72d1fdd8a4551527350f3637f1c03933d10f9489
Laura Rodriguez <la.rodriguez@uniandes.edu.co>
2022-05-14 01:11:32 -0500

    modulo de confidencialidad completo

--- Educacion_Estrella/reservas/views.py
+++ Educacion_Estrella/reservas/views.py
@@ -3,19 +3,36 @@ from django.http import HttpResponse
 from django.core import serializers
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
+
 
 
 @csrf_exempt
 def reserva_view(request, pk):
     if request.method == 'GET':
-        reserva_dto = vl.get_variables(pk)
-        reserva = serializers.serialize('json', reserva_dto,)
-        return HttpResponse(reserva, 'application/json')
+        user = authenticate(username='test', password='test1234')
+        if user is not None:
+            reserva_dto = vl.get_variables(pk)
+            reserva = serializers.serialize('json', reserva_dto,)
+            return HttpResponse(reserva, 'application/json')
+        else:
+            print("Ha ocurrido un error, por favor inicie sesion")
+            return HttpResponse("Ha ocurrido un error, por favor inicie sesion")
    
 @csrf_exempt
 def reservas_view(request):
-   
-    reservas_dto = vl.get_muchas()
-    reservas = serializers.serialize('json', reservas_dto,)
-    return HttpResponse(reservas, 'application/json')
+    user = authenticate(username='test', password='test1234')
+    print(user)
+    if user is not None:
+        print("entro")
+        reservas_dto = vl.get_muchas()
+        reservas = serializers.serialize('json', reservas_dto,)
+        return HttpResponse(reservas, 'application/json')
+
+    else:
+        # print("Ha ocurrido un error, por favor inicie sesion")
+        # return HttpResponse("Ha ocurrido un error, por favor inicie sesion")
+        reservas_dto = vl.get_muchas()
+        reservas = serializers.serialize('json', reservas_dto,)
+        return HttpResponse(reservas, 'application/json')
 
