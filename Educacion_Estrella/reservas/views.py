1a3f9caf1405faf88f75521d052e8b4fa45920a7
Wilton2612 <wiltonestebanm2001@gmail.com>
2022-05-14 08:16:44 -0500

    others changes

--- Educacion_Estrella/reservas/views.py
+++ Educacion_Estrella/reservas/views.py
@@ -15,41 +15,20 @@ from django.contrib.auth.decorators import login_required
 
 @csrf_exempt
 def reserva_view(request, id=0):
-    if request.method == 'GET':
-        user = authenticate(username='test', password='test1234')
-        if user is not None:
-            reserva = vl.get_reservas(id)
-            context = {
-                'reserva': reserva
-            }
-            return render(request, 'unico.html', context)
+    reserva = vl.get_reservas(id)
+    context = {
+        'reserva': reserva
+    }
+    return render(request, 'unico.html', context)
 
            
-        else:
-            print("Ha ocurrido un error, por favor inicie sesion")
-            return HttpResponse("Ha ocurrido un error, por favor inicie sesion")
-   
-
-
 
 @csrf_exempt
 def reservas_view(request):
-    user = authenticate(username='test', password='test1234')
-    print(user)
-    if user is not None:
-        print("entro")
-        reservas = vl.get_muchas()
-        context = {
-        'reserva_list': reservas
-        }
-        return render(request, 'reservas.html', context)
-        
-    else:
-        # print("Ha ocurrido un error, por favor inicie sesion")
-        # return HttpResponse("Ha ocurrido un error, por favor inicie sesion")
-        reservas = vl.get_muchas()
-        context = {
-        'reserva_list': reservas
-        }
-        return render(request, 'reservas.html', context)
+    reservas = vl.get_muchas()
+    context = {
+    'reserva_list': reservas
+    }
+    return render(request, 'reservas.html', context)
+    
 
